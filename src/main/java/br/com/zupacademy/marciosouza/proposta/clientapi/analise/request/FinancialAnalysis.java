3a9dc5ebe867e1b5bf32ddbacd4b16148a7cfa66
mfkldev <marcio.souza@zup.com.br>
2021-07-21 13:00:07 -0300

    melhorando_visibilidade_healthcheck
    Objetivo
    
    Nossa aplicação deve "mostrar" a saúde dela para algum sistema automático de monitoramento ou equipe de operação!
    
    Resultado Esperado
    
    Criação de um endpoint HTTP (REST) que "informe" a saúde da nossa aplicação.
    
    API deve retornar o status code 200 quando tudo estiver ok
    API deve retornar o status code 5xx quando algum componente de nossa infraestrutura estiver com mal-funcionamento (ex: banco de dados ou algum broker de mensagens)

--- src/main/java/br/com/zupacademy/marciosouza/proposta/clientapi/analise/request/FinancialAnalysis.java
+++ src/main/java/br/com/zupacademy/marciosouza/proposta/clientapi/analise/request/FinancialAnalysis.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "solicitacao", url = "http://localhost:9999/api")
+@FeignClient(value = "requestFinancialAnalysis", url = "http://localhost:9999/api")
 public interface FinancialAnalysis {
 
     @RequestMapping(method = RequestMethod.POST, value = "/solicitacao")
