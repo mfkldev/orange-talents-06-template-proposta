c47b37d92cf929cc0c2ff7923850b07847ae6d7a
mfkldev <marcio.souza@zup.com.br>
2021-07-27 00:53:39 -0300

    login_via_senha

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM amazoncorretto:11-alpine-jdk
 MAINTAINER Marcio Franklin
-COPY target/proposta-0.4.0.jar proposta-0.4.0.jar
-ENTRYPOINT ["java","-jar","/proposta-0.4.0.jar"]
+COPY target/proposta-0.5.0.jar proposta-0.5.0.jar
+ENTRYPOINT ["java","-jar","/proposta-0.5.0.jar"]
--- src/main/java/br/com/zupacademy/marciosouza/proposta/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/zupacademy/marciosouza/proposta/config/security/SecurityConfigurations.java
@@ -1,58 +1,30 @@
 package br.com.zupacademy.marciosouza.proposta.config.security;
 
-import br.com.zupacademy.marciosouza.proposta.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 @Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private AuthenticationService authenticationService;
-	
-	@Autowired
-	private TokenService tokenService;
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Override
-	@Bean
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.anyRequest().authenticated()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new TokenAuthenticationFilter(tokenService, userRepository), UsernamePasswordAuthenticationFilter.class);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+		http.authorizeRequests(authorizeRequests -> authorizeRequests
+				.antMatchers(HttpMethod.GET, "/proposta/**").hasAuthority("SCOPE_adm")
+				.antMatchers(HttpMethod.POST, "/proposta/**").hasAuthority("SCOPE_adm")
+				.antMatchers(HttpMethod.POST, "/biometria/**").hasAuthority("SCOPE_adm")
+				.antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_adm"))
+				.cors()
+				.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().csrf().disable()
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 	}
 }
